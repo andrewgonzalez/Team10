937d585b9be982d103f6c245d8decc779d987076
Carlos Miguel Sayao <carsayao@pdx.edu>
2018-11-05 15:58:15 -0800

    branch for BST

--- src/BST.h
+++ src/BST.h
@@ -1,8 +1,18 @@
 //
-// Created by thisSpectre on 11/5/2018.
+// Created by Miguel on 11/5/2018.
 //
 
 #ifndef TEAM10_BST_H
 #define TEAM10_BST_H
 
-#endif //TEAM10_BST_H
+class BST
+{
+    public:
+        BST();
+        ~BST();
+        int add(int data);
+    private:
+};
+
+
+#endif TEAM10_BST_H
