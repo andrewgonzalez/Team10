cdeed61d2f1b42d899335364842e44bb5e2d7d05
Carlos Miguel Sayao <carsayao@pdx.edu>
2018-11-06 18:38:05 -0800

    BST branch

--- src/BST.cpp
+++ src/BST.cpp
@@ -1,4 +1,50 @@
 //
-// Created by thisSpectre on 11/5/2018.
+// Created by Miguel on 11/5/2018.
 //
 
+#include "BST.h"
+
+
+
+// Initialize variables
+Node::Node()
+{
+    data = NULL;
+    left = NULL;
+    right = NULL;
+}
+
+// Delete node
+Node::~Node()
+{
+    data = NULL;
+    left = NULL;
+    right = NULL;
+}
+
+int Node::add(int data_in)
+{
+    if ( data_in )
+        data = data_in;
+    else
+        return 0;
+    return 1;
+}
+
+int Node::display()
+{
+    if ( data )
+        cout <<data <<endl;
+}
+
+// Initialize tree to NULL
+BST::BST()
+{
+    root = NULL;
+}
+
+// Delete tree
+BST::~BST()
+{
+
+}
--- src/BST.h
+++ src/BST.h
@@ -2,19 +2,33 @@
 // Created by Miguel on 11/5/2018.
 //
 
+#ifndef BST_H
+#define BST_H
+
 #include <iostream>
 
-#ifndef TEAM10_BST_H
-#define TEAM10_BST_H
+class Node
+{
+public:
+    Node();
+    ~Node();
+    int add(int data_in);
+    int display();
+private:
+    int data;
+    Node * left;
+    Node * right;
+
+};
 
 class BST
 {
-    public:
-        BST();
-        ~BST();
-        int add(int data);
-    private:
+public:
+    BST();
+    ~BST();
+    int add(int data);
+private:
+    Node * root;
 };
 
-
-#endif TEAM10_BST_H
+#endif BST_H
