26a5d1ee957cf28eb23649dd31db0a246eeb9bc3
Carlos Miguel Sayao <carsayao@pdx.edu>
2018-11-06 18:20:32 -0800

    BST.h and *.cpp

--- src/BST.h
+++ src/BST.h
@@ -1,8 +1,21 @@
 //
-// Created by thisSpectre on 11/5/2018.
+// Created by Miguel on 11/5/2018.
 //
 
-#ifndef TEAM10_BST_H
-#define TEAM10_BST_H
 
-#endif //TEAM10_BST_H
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+
+class BST
+{
+    public:
+        BST();
+        ~BST();
+        int add(int data);
+    private:
+        Node * root;
+};
+
+#endif BST_H
