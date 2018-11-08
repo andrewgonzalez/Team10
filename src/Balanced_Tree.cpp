b58a45583bf647102a851b321e149563f583c74c
Carlos Miguel Sayao <carsayao@pdx.edu>
2018-11-07 22:16:36 -0800

    Added Balanced_Tree.h and *.cpp, derived from BST

---
+++ src/Balanced_Tree.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Carlos Miguel Sayao on 07-Nov-18.
+//
+
+#include "Balanced_Tree.h"
+
+using namespace std;
+
+Balanced_Tree::Balanced_Tree()
+{
+    this.root = nullptr;
+    this.balance = 0;
+}
+
+Balanced_Tree::~Balanced_Tree()
+{
+
+}
---
+++ src/Balanced_Tree.h
@@ -0,0 +1,28 @@
+//
+// Created by Carlos Miguel Sayao on 07-Nov-18.
+//
+
+#ifndef BALANCED_TREE_H
+#define BALANCED_TREE_H
+
+#include "BST.h"
+
+class Balanced_Tree: public BST
+{
+    public:
+
+        Balanced_Tree();
+        ~Balanced_Tree();
+
+        void insert( int num );
+        void insert( Node c, int num );
+
+
+    private:
+
+        // Keep track of balance of particular node
+        int balance;
+
+};
+
+#endif BALANCED_TREE_H
