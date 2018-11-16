40af683ceb7e68075485d9bff45a0ab93c993c1d
Jason Zhang <43938722+Zhi22@users.noreply.github.com>
2018-11-16 15:38:35 -0800

    Add files via upload

---
+++ Terminal.h
@@ -0,0 +1,17 @@
+//Terminal.h
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Terminal
+{
+  public:
+    void login_menu();
+    void provider_menu();
+    void manager_menu();
+    void operator_menu();
+    void manager_operator_menu();
+  
+};
