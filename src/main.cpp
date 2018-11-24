6d924c2afd1783a390fbf0fed0f074ecd579d9db
Andrew Gonzalez <andrew.gonzal@gmail.com>
2018-11-23 21:07:01 -0800

    Copy pasted code into main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
-#include"Terminal.h"
+#include "Terminal.h"
+#include "Balanced_Tree.h"
+#include "Person.h"
 
 using namespace std;
 
 int main()
 {
-  Terminal object;
-  object.login_menu();
-  return 0;
+    string p11;
+    p11 = "Carlos Miguel Sayao";
+    string p12;
+    p12 = "11020 NE Marx St";
+    string p13;
+    p13 = "Portland";
+    string p14;
+    p14 = "OR";
+    Person p1 = Person(932082330, 97220, p11, p12, p13, p14);
+
+    tNode *node;
+    //Person p1 = Person(932082330, 97220, "Carlos", "Marx St", "Portland", "OR");
+    node = &p1;
+    node->display();
+
+    BST bst;
+
+
+
+    return 1;
 }
 
