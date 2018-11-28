fe6cb3f0e8623c3bf654f9cf7a02565e0afb06f0
Andrew Gonzalez <andrew.gonzal@gmail.com>
2018-11-27 19:23:29 -0800

    Added file main.cpp to create an entry point for the application.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Andrew Gonzalez on 11/27/18.
+//
+
+#include "Terminal.h"
+
+int main(int argc, char *argv[]) {
+    Terminal userInterface;
+
+    userInterface.login_menu();
+    return 0;
+}
